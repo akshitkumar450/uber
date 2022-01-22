ded8dbe3e92adba4e4c00d32ae53d5e09ddbfe59
Akshit Kumar <akshitkumar450@gmail.com>
2022-01-22 20:08:38 +0530

    saving the search data to redux

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -2,8 +2,10 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import tw from "tailwind-react-native-classnames";
-
+import { useDispatch } from "react-redux";
+import { setOrigin, setDestination } from "../Redux/slices/navSlice";
 const SearchBar = () => {
+  const dispatch = useDispatch();
   return (
     <View>
       <GooglePlacesAutocomplete
@@ -21,6 +23,15 @@ const SearchBar = () => {
         onPress={(data, details = null) => {
           // 'details' is provided when fetchDetails = true
           console.log(data, details);
+          //   saving the origin from search field to redux
+          dispatch(
+            setOrigin({
+              location: details.geometry.location,
+              description: data.description,
+            })
+          );
+          //   set the destination to null
+          dispatch(setDestination(null));
         }}
         fetchDetails={true} //include geometry location,cordinates
         // to hide powered by text which will be available in details
@@ -29,6 +40,10 @@ const SearchBar = () => {
         nearbyPlacesAPI="GooglePlacesSearch"
         debounce={400} //to search after 400s after stopping typing
         placeholder="from where?"
+        query={{
+          key: "YOUR API KEY",
+          language: "en",
+        }}
       />
     </View>
   );
